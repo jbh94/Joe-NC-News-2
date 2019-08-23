5d0e15be269390939d98dae63d1f1fef76fdc2be
Joe <radiob0x101@gmail.com>
2019-08-23 12:41:49 +0100

    setup seeding for test dev

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS joe_nc_news;
 DROP DATABASE IF EXISTS joe_nc_news_test;
+DROP DATABASE IF EXISTS joe_nc_news;
 
-CREATE DATABASE joe_nc_news;
 CREATE DATABASE joe_nc_news_test;
+CREATE DATABASE joe_nc_news;
