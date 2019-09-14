9464149fcd207061c35c867c0e473ed6c2d64cca
Joe <radiob0x101@gmail.com>
2019-09-15 00:04:22 +0100

    ready for deployment to heroku

--- db/data/index.js
+++ db/data/index.js
@@ -4,6 +4,6 @@ const testData = require('./test-data');
 
 const devData = require('./development-data');
 
-const data = { development: devData, test: testData };
+const data = { development: devData, test: testData, production: devData };
 
 module.exports = data[ENV];
--- listen.js
+++ listen.js
@@ -1,12 +1,9 @@
 const app = require('./app');
 
-app.listen(9090, () => {
-  console.log('Server is listening on port 9090...');
-});
-
-// const PORT = process.env.PORT || 9090;
-
-// app.listen(PORT, err => {
-//   if (err) throw err;
-//   console.log(`Server listening on port ${PORT}...`);
+// app.listen(9090, () => {
+//   console.log('Server is listening on port 9090...');
 // });
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
